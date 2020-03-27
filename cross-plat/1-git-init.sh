115a84adad1bf8a43ec64905b6c58f6b260f91e2
JasonCH Lu <jasonch_lu@moxa.com>
2020-03-27 17:05:08 +0800

    Add alias co to global git config.

--- cross-plat/1-git-init.sh
+++ cross-plat/1-git-init.sh
@@ -2,7 +2,8 @@
 
 # customize git config globally
 git config --global user.name "JasonCH Lu"
-git config --global user.email j3hb.lu@gmail.com
+git config --global user.email jasonch_lu@moxa.com
 git config --global core.editor vim
 git config --global merge.tool vimdiff
 git config --global alias.xlog "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+git config --global alias.co "checkout"
