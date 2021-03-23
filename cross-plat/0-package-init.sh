9f3e0517f2a7b6e35be4196ae6fa80d128090b49
JasonCH Lu <jasonch_lu@moxa.com>
2021-03-23 15:42:00 +0800

    ctags package name change.

--- cross-plat/0-package-init.sh
+++ cross-plat/0-package-init.sh
@@ -5,4 +5,4 @@ sudo apt-get upgrade -y
 # update the package list
 sudo apt-get update -y
 # install required packages
-sudo apt-get install vim ssh git git-flow svn mtd-utils tftp tftpd xinetd libncurses5-dev ctags cscope tmux -y
+sudo apt-get install vim ssh git git-flow svn mtd-utils tftp tftpd xinetd libncurses5-dev exuberant-ctags cscope tmux -y
