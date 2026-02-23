ae710f5384845c423a8da21a6f20ff763f0d6957
JasonCH_Lu <jasonch.lu@moxa.com>
2026-02-23 14:26:29 +0800

    update git alias commands

--- cross-plat/1-git-init.sh
+++ cross-plat/1-git-init.sh
@@ -5,5 +5,7 @@ git config --global user.name "JasonCH Lu"
 git config --global user.email jasonch_lu@moxa.com
 git config --global core.editor vim
 git config --global merge.tool vimdiff
-git config --global alias.plog "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
+git config --global alias.lg "log --graph --abbrev-commit --decorate --date=relative --format=format:'%C(bold blue)%h%C(reset) - %C(bold green)(%ar)%C(reset) %C(white)%s%C(reset) %C(dim white)- %an%C(reset)%C(bold yellow)%d%C(reset)' --all"
 git config --global alias.co "checkout"
+git config --global alias.cp "cherry-pick"
+git config --global alias.fo "fetch origin"
